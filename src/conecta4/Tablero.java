fb6bbdd83801dfc2966b3771fbca8f2d58c0e2a4
Mañana <Maana@DESKTOP-QOPA2UO>
2023-03-14 12:24:52 +0100

    Conecta4

---
+++ src/conecta4/Tablero.java
@@ -0,0 +1,28 @@
+package conecta4;
+
+public class Tablero {
+	
+	public int[] donde;
+	public char[][] tablero;
+	
+	public Tablero() {
+		
+		tablero = new char[6][7];
+		donde = new int[7];
+		
+		for (int i = 0; i < donde.length; i++) {
+			
+			donde[i] = 7;
+		}
+	}
+
+	public void coloca(int donde) {
+		
+		if (donde ) {
+			
+		}
+		
+		
+	}
+
+}
