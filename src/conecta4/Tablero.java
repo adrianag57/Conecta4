df9dd95a6605e3853cb51895c43d1d29f5be9c68
Mañana <Maana@DESKTOP-QOPA2UO>
2023-03-16 09:20:34 +0100

    Conecta4_2

--- src/conecta4/Tablero.java
+++ src/conecta4/Tablero.java
@@ -1,28 +1,66 @@
 package conecta4;
 
 public class Tablero {
-	
-	public int[] donde;
-	public char[][] tablero;
-	
+
+	private int[] donde;
+	private char[][] tablero;
+
+	private final int FILAS = 6;
+	private final int COLUMNAS = 7;
+
+	private char turno;
+
 	public Tablero() {
-		
-		tablero = new char[6][7];
-		donde = new int[7];
-		
-		for (int i = 0; i < donde.length; i++) {
-			
-			donde[i] = 7;
+
+		turno = 'X';
+		tablero = new char[FILAS][COLUMNAS];
+		donde = new int[COLUMNAS];
+
+		for (int i = 0; i < COLUMNAS; i++) {
+
+			donde[i] = FILAS - 1;
 		}
 	}
 
-	public void coloca(int donde) {
-		
-		if (donde ) {
-			
+	public void coloca(int columna) {
+
+		if ((columna >= 0) & (columna < COLUMNAS)) {
+
+			if (donde[columna] >= 0) {
+
+				int fila = donde[columna];
+				tablero[fila][columna] = turno;
+				donde[columna]--;
+				turno = (turno == 'X') ? 'O' : 'X';
+			}
 		}
-		
-		
+
+	}
+
+	@Override
+	public String toString() {
+
+		String devolver = "";
+
+		for (int i = 0; i < FILAS; i++) {
+
+			for (int j = 0; j < COLUMNAS; j++) {
+
+				devolver += tablero[i][j] + " ";
+			}
+			devolver += "\n";
+		}
+		for (int j = 0; j < COLUMNAS; j++) {
+
+			devolver += "- ";
+		}
+		devolver += "\n";
+		for (int j = 0; j < COLUMNAS; j++) {
+
+			devolver += j + " ";
+		}
+
+		return devolver;
 	}
 
 }
